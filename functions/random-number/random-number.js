5e581c98ab13d9be365569a5fdbdc739cf3c12e0
Rudolf Hersén <rudolf.hersen@schibsted.se>
2019-10-16 22:03:20 +0200

    Tweak function random-number

--- functions/random-number/random-number.js
+++ functions/random-number/random-number.js
@@ -1,9 +1,10 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
+    let n = 100 * Math.random()
     return {
       statusCode: 200,
-      body: "17"
+      body: n.toString()
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
